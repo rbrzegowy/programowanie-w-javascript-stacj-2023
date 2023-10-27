ce41ea971120c8d5c3fc3348426da783c3fc22fe
RB <r.brzegowy@gmail.com>
2023-10-27 09:59:00 +0200

    lab2

--- Lab 2 - slider/main.js
+++ Lab 2 - slider/main.js
@@ -1,10 +1,13 @@
 // notatnik z zajęć
 
 const main = document.querySelector('main')
+const slides = document.querySelector('.slides')
 
 // jednorazowe wykonanie kodu po określonym czasie
 const timeoutRef = setTimeout(
     () => {
+        slides.style.transform = 'translateX(-100px)'
+        // slides.classList.add('slide2')
         main.innerHTML = 'Msg from setTimeout'
     },
     2000
